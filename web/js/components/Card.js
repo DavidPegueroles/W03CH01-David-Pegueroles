1ce97bc57d137470d069a37847a2f71193903d46
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-01-24 23:16:15 +0100

    Added half of daenerys card

--- web/js/components/Card.js
+++ web/js/components/Card.js
@@ -1,17 +1,38 @@
 class Card {
   element;
 
-  constructor(parentElement, className, htmlTag) {
+  constructor(parentElement, className, htmlTag, imageSource) {
     this.element = document.createElement(htmlTag);
     this.element.classList = className;
 
     parentElement.append(this.element);
 
-    this.contentHTML();
+    this.createDiv();
+    this.image(imageSource);
+    this.divInside();
   }
 
-  contentHTML() {
-    this.element.innerHTML = ``;
+  createDiv() {
+    this.div = document.createElement("div");
+    this.div.classList = "card character__card";
+
+    this.element.append(this.div);
+  }
+
+  image(imageSource) {
+    this.img = document.createElement("img");
+    this.img.classList = "character__picture card-img-top";
+    this.img.src = imageSource;
+    this.img.alt = "Nombre y familia del personaje";
+
+    this.div.append(this.img);
+  }
+
+  divInside() {
+    this.divBody = document.createElement("div");
+    this.divBody.classList = "card-body";
+
+    this.element.append(this.divBody);
   }
 }
 
--- web/js/index.js
+++ web/js/index.js
@@ -1,5 +1,7 @@
+import Card from "./components/Card.js";
 import ReyCard from "./components/ReyCard.js";
 
 const pare = document.querySelector(".characters-list");
 
 const characterCard = new ReyCard(pare, "character col", "li");
+const newLuchador = new Card(pare, "character col", "li", "img/daenerys.jpg");

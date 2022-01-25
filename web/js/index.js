1278e0c965a121a2690d20c661c10b342c783757
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-01-25 19:13:10 +0100

    Added ButtonComponent

--- web/js/index.js
+++ web/js/index.js
@@ -1,10 +1,13 @@
+import ButtonComponent from "./components/ButtonComponent.js";
 import Component from "./components/Component.js";
 import ImageComponent from "./components/ImageComponent.js";
 
 const parentElement = document.querySelector("body");
 
-const abersifunciona = new ImageComponent(
-  parentElement,
-  "img/jaime.jpg",
-  "jaime es"
-);
+const image = new ImageComponent(parentElement, "img/jaime.jpg", "jaime es");
+
+const action = () => {
+  console.log("aa");
+};
+
+const dedAf = new ButtonComponent(parentElement, "DED AF", action());

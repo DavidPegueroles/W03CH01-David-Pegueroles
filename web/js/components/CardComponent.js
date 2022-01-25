e1707b682e7ac3ed463f442d2b130de3b73c4766
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-01-25 18:09:56 +0100

    Added CardComponent

---
+++ web/js/components/CardComponent.js
@@ -0,0 +1,9 @@
+import Component from "./Component";
+
+class CardComponent extends Component {
+  constructor(parentElement, HTMLTag) {
+    super(parentElement, HTMLTag);
+  }
+}
+
+export default CardComponent;
